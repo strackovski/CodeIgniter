37c228e753b8c888924f8a404f40e9d9d09d3929
strackovski <vladimir.strackovski@gmail.com>
2014-07-22 14:17:29 +0000

    Change default templates path.

--- application/config/twig.php
+++ application/config/twig.php
@@ -1,3 +1,3 @@
 <?php if (!defined('BASEPATH')) exit('No direct script access allowed');
-$config['template_dir'] = FCPATH.'templates';
+$config['template_dir'] = FCPATH.'web/templates';
 $config['cache_dir'] = FCPATH.'var/cache/twig';
